0e6c45ae4e3245fa2236458d4d9469979660b6a5
mohankrishna1993 <mohan.vannemareddy94@gmail.com>
2023-09-14 16:57:13 +0530

    to compute the sum of the two given integers. If the two values are the same, then return triple their sum.

---
+++ compute the sum of the two given integers.js
@@ -0,0 +1,12 @@
+//compute the sum of the two given integers. If the two values are the same, then return triple their sum.
+
+
+function triple(x,y) {
+     if(x == y) {
+     return 3*(x+y);
+     }
+	 else 
+	 {
+     return x + y;
+     }
+   }
