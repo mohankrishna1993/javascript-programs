11c2f6469242f155e90508343137b755b5d714b5
mohankrishna1993 <mohan.vannemareddy94@gmail.com>
2023-09-14 16:48:12 +0530

    Difference between a given number

---
+++ Difference bw numbers.js
@@ -0,0 +1,11 @@
+
+/* Write a JavaScript program to get the difference between a given number and 13
+if the number is broader than 13 return double the absolute difference.*/
+
+function difference(n)
+ {
+    if (n <= 13)
+        return 13 - n;
+    else
+        return (n - 13) * 2;
+ }
