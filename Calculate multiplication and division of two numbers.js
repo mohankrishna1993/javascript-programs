eadb2e87678be7f26e451bcad1d99088732c41fa
mohankrishna1993 <mohan.vannemareddy94@gmail.com>
2023-09-14 14:49:02 +0530

    multiplication and division of two numbers

---
+++ Calculate multiplication and division of two numbers.js
@@ -0,0 +1,12 @@
+function multiply(mul1,mul2) {
+
+   var result = mul1 * mul2 
+   return result;
+}
+multiply(2,3)
+
+function division(div1,div2) {
+
+   var result = div1 / div2 
+   return result;
+}
