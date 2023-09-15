cab9a7b839ef99d36c335c4c8802d7dbfec278fa
mohankrishna1993 <mohan.vannemareddy94@gmail.com>
2023-09-15 15:51:37 +0530

    string changing the position of first and last characters

---
+++ string changing the position of first and last characters.js
@@ -0,0 +1,11 @@
+//Create a new string from a given string changing the position of first and last characters
+
+function first_last(str1) 
+  {
+  if (str1.length <= 1)
+  {
+    return str1;
+  }
+  mid_char = str1.substring(1, str1.length - 1);
+  return (str1.charAt(str1.length - 1)) + mid_char + str1.charAt(0);
+}
